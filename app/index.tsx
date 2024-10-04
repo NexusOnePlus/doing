d09be8876b8e619f8f5523f7718129514cd21abf
Nexus <wv35788@gmail.com>
2024-10-04 11:28:56 -0500

    Button added + (does nothing)

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ImageBackground, StyleSheet } from "react-native";
+import { View, Text, ImageBackground, StyleSheet, Pressable } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import * as NavigationBar from 'expo-navigation-bar';
 
@@ -10,22 +10,48 @@ NavigationBar.setBackgroundColorAsync("black");
 const HomeScreen = () => {
     return (
         <View style={styles.root}>
-            <StatusBar style="light" backgroundColor="black"/>
-             <ImageBackground source={wallpaper} style={styles.container}>
+            <StatusBar style="light" backgroundColor="black" />
+            <ImageBackground source={wallpaper} style={styles.container}>
+                <View style={styles.rootow}>
                 <Text style={styles.text} >Hola</Text>
-             </ImageBackground>
+                </View>
+                <View style={styles.low}>
+                    <Pressable style={styles.button} >
+                        <Text style={styles.text}>+</Text>
+                    </Pressable>
+                </View>
+            </ImageBackground>
         </View>
     );
 }
 
 
 const styles = StyleSheet.create({
-    root: {flex: 1},
+    root: { flex: 1 },
+    rootow: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    low: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
     container: {
         flex: 1, justifyContent: 'center',
     },
     text: {
         textAlign: 'center', fontSize: 25, color: 'white',
+    },
+    button: {
+        justifyContent: 'center',
+        backgroundColor: 'red',
+        alignItems: 'center',
+        width: 60,
+        height: 60,
+        borderRadius: 12,
+        elevation: 3,
     }
 });
 export default HomeScreen
